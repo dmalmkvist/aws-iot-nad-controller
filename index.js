9a6616b98f7cdfd0874262f2539be848734958ba
Daniel Malmkvist <daniel@malmkvist.info>
2018-07-10 20:30:17 +0200

    better console logging and added a startup script

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
 #!/usr/bin/env node
+require('rconsole')
 const CmdParser = require('./src/cmd-parser');
 const IotShadow = require('./src/iot-shadow');
 const Controller = require('./src/controller');
 
+process.on('SIGINT', quit);
+process.on('SIGTERM', quit);
+
+console.set({
+  facility: 'local0',
+  title: 'nad-controller',
+  showLine: false});
+
 let cmdParser = new CmdParser(process.argv.slice(2));
-let iotShadow = new IotShadow(cmdParser);
 
-iotShadow.connect(function() {
-  console.log('Connected to shadow');
-  let controller = new Controller(iotShadow);
-  controller.on('failure', (error) => console.log('ERROR: ' + error));
-  // controller.on('delta', (data) => console.log('DELTA: ', data));
-  // controller.on('state-change', (data) => console.log('STATE: ', data));
-  // controller.on('device-change', (data) => console.log('DEVICE: ', data));
+function init(callback) {
+  let iotShadow = new IotShadow(cmdParser);
 
-  controller.connect((error) => {
+  iotShadow.connect(function() {
+    console.log('Connected to shadow');
+    let controller = new Controller(iotShadow);
+
+    controller.on('failure', (error) => console.log('ERROR: ' + error));
+    controller.connect(callback);
+  })
+}
+
+
+function start() {
+  init((error) => {
     if (error) {
-      console.error(error);
-      process.exit(1);
+      console.log(error);
+      setTimeout(start, 1000);
+      return;
     }
 
     console.log('Controller up and running!')
-  });
-})
 
+  });
+}
+
+function quit(signal) {
+  console.log(`Received ${signal}, exiting`);
+  process.exit(0);
+}
+
+console.log('Starting nad-controller');
+try {
+  start();
+} catch(error) {
+  console.log('Failed to start: ' + error);
+}
